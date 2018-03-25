41e9a8332a40009e0b54d401ee9f6a6316886bf9
ishotjr <github@ishotjr.com>
2018-03-24 22:57:47 -0400

    :octocat: :tada: Initial commit

---
+++ Gust-META.ino
@@ -0,0 +1,17 @@
+#include <Gamebuino-Meta.h>
+
+void setup() {
+
+  gb.begin();
+
+}
+
+void loop() {
+  
+  while(!gb.update());
+  
+  gb.display.clear();
+  
+  gb.display.println("TODO: everything!");
+  
+}
